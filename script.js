e3e73ae5e9ec9548562367319339d151d47f65d0
Andrea Barbuto <barbutoandrea99@gmail.com>
2025-12-05 16:16:53 +0100

    feat: completed Pali exercise

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
 /* Chiedere all’utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma */
 
+let userWord = prompt("Inserisci una parola");
+
+console.log(wordIsPali(userWord));
+
+
+function wordIsPali (string)
+{
+    string = string.toLowerCase();
+    let toArray = string.split("");
+    console.log(toArray);
+    toArray = toArray.reverse().join("");
+    console.log(toArray);
+    if (toArray === string)
+    {
+        return "La parola scelta è palindroma";
+    }
+    
+    else 
+    {
+        return "La parola scelta non è palindroma";
+    }
+    console.log(toArray);
+}
