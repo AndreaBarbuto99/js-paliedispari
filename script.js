add504f011c311a645fa7dff6b6cc07ed94b9896
Andrea Barbuto <barbutoandrea99@gmail.com>
2025-12-05 17:14:30 +0100

    feat: added ParioDispari exercise

--- script.js
+++ script.js
@@ -1,10 +1,54 @@
-/* Chiedere all’utente di inserire una parola
-Creare una funzione per capire se la parola inserita è palindroma */
+// /* Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma */
 
 let userWord = prompt("Inserisci una parola");
 
 console.log(wordIsPali(userWord));
 
+// /*L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+// Sommiamo i due numeri
+// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+// Dichiariamo chi ha vinto. */
+
+
+const userPD = prompt("La somma dei numeri deve essere pari o dispari?");
+let userNum = 0;
+let sumNum;
+
+while (userNum < 1 || userNum > 5)
+{
+    userNum = parseInt(prompt("Inserisci il numero da te scelto in un range da 1 a 5"));
+}
+
+console.log(userNum);
+
+const botNum = Math.floor(Math.random() * 5) + 1;
+console.log(botNum);
+
+sumNum = userNum + botNum;
+console.log(sumNum);
+
+console.log(isOddEven(sumNum, userPD));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// FUNZIONI
 
 function wordIsPali (string)
 {
@@ -23,4 +67,16 @@ function wordIsPali (string)
         return "La parola scelta non è palindroma";
     }
     console.log(toArray);
+}
+
+function isOddEven (sum, oddEven) {
+    if (sum % 2 === 0 && oddEven === "pari"){
+        return "Hai vinto";
+    }
+    else if(sum % 2 !== 0 && oddEven === "dispari"){
+        return "Hai vinto";
+    }
+    else {
+        return "Hai perso";
+    }
 }
