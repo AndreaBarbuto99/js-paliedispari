a4afddaa1fc912fadb3461dc816f9441c76464b7
Andrea Barbuto <barbutoandrea99@gmail.com>
2025-12-05 17:40:39 +0100

    feat: aggiunto metodo manuale senza funzioni esterne per la verifica della parola palindroma

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-// /* Chiedere all’utente di inserire una parola
-// Creare una funzione per capire se la parola inserita è palindroma */
-
+// // // /* Chiedere all’utente di inserire una parola
+// // // Creare una funzione per capire se la parola inserita è palindroma */
+// // CHIAMATA A FUNZIONE PER VERIFICARE SE UNA STRINGA E' PALINDROMA
 let userWord = prompt("Inserisci una parola");
-
 console.log(stringIsPali(userWord));
 
-// /*L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
-// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
-// Sommiamo i due numeri
-// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
-// Dichiariamo chi ha vinto. */
+// // // /*L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+// // // Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+// // // Sommiamo i due numeri
+// // // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+// // // Dichiariamo chi ha vinto. */
 
 
 const userPD = prompt("La somma dei numeri deve essere pari o dispari?");
@@ -32,9 +31,9 @@ console.log(sumNum);
 
 console.log(isOddEven(sumNum, userPD));
 
-
-
-
+// CHIAMATA A FUNZIONE PER VERIFICARE SE UNA PAROLA E' PALINDROMA (METODO 2)
+// let userWordTwo = prompt("Inserisci una parola");
+// console.log(stringIsPaliMe(userWordTwo));
 
 
 
@@ -51,6 +50,7 @@ console.log(isOddEven(sumNum, userPD));
 
 // FUNZIONI
 
+// FUNZIONE PER VERIFICARE SE UNA STRINGA E' PALINDROMA (METODO CON FUNZIONI ESTERNE)
 function stringIsPali (string)
 {
     string = string.toLowerCase();
@@ -71,6 +71,30 @@ function stringIsPali (string)
     console.log(toArray);
 }
 
+// FUNZIONE PER VERIFICARE SE UNA STRINGA E' PALINDROMA (METODO CON ORDINAMENTO MANUALE)
+
+function stringIsPaliMe (string)
+{
+    string = string.toLowerCase();
+    let reversedString = "";
+    let result;
+    for (let i = string.length - 1; i >= 0; i--) {
+        reversedString = reversedString + string[i];
+    }
+    if (reversedString === string)
+    {
+        return result = "La parola scelta è palindroma";
+    }
+    
+    else 
+    {
+        return result = "La parola scelta non è palindroma";
+    }
+    console.log(reversedString);
+}
+
+// FUNZIONE PER VERIFICARE SE LA SOMMA DI DUE NUMERI E' PARI O DISPARI. RITORNA RISULTATO POSITIVO SE USER HA FATTO LA SCELTA GIUSTA
+
 function isOddEven (sum, oddEven) {
     let result;
     if (sum % 2 === 0 && oddEven === "pari"){
@@ -83,4 +107,3 @@ function isOddEven (sum, oddEven) {
         return result = "Hai perso";
     }
 }
-
